de8219e134f0276c821459abf918942c7b6193cf
Nicholas Papierniak <nick.papierniak@gmail.com>
2024-04-16 10:00:46 -0500

    Added new screen

---
+++ Mega Jump/StartScreen.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIView.swift
+//  Mega Jump
+//
+//  Created by Nicholas Papierniak on 4/16/24.
+//
+
+import SpriteKit
+import GameplayKit
+
+class StartScreen: SKScene {
+    override func didMove(to view: SKView) {
+        
+        
+    }
+}
+
