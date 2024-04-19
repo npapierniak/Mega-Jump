47b266ee14ae0ef69df82f76befdf4491768efa3
Nicholas Papierniak <nick.papierniak@gmail.com>
2024-04-19 09:57:48 -0500

    Added a background

--- Mega Jump/GameViewController.swift
+++ Mega Jump/GameViewController.swift
@@ -10,15 +10,14 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
+            if let scene = SKScene(fileNamed: "StartScene") {
                 // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
+                scene.scaleMode = .resizeFill
                 
                 // Present the scene
                 view.presentScene(scene)
--- Mega Jump/StartScreen.swift
+++ Mega Jump/StartScreen.swift
@@ -8,10 +8,25 @@
 import SpriteKit
 import GameplayKit
 
-class StartScreen: SKScene {
+class StartScreen: SKScene, SKPhysicsContactDelegate {
     override func didMove(to view: SKView) {
-        
-        
+        createBackground()
+    }
+    func createBackground() {
+        let imageName = "f0b8d6fc41d58e868b348cc33881093f.jpg"
+        let stars = SKTexture (imageNamed: imageName)
+        for i in 0...1 {
+            let starsBackground = SKSpriteNode (texture: stars)
+            let scaleX = size.width / starsBackground.size.width
+            let scaleY = size.height / starsBackground.size.height
+            let scaleFactor = max(scaleX, scaleY)
+            starsBackground.setScale(scaleFactor)
+            starsBackground.zPosition = -1
+            starsBackground.position = CGPoint(x: 0, y: starsBackground.size.height * CGFloat (i))
+            addChild(starsBackground)
+            
+        }
     }
 }
+
 
