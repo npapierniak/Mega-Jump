d3b63a1c2ec6742a284473b61d74022b8811c6f2
Nicholas Papierniak <nick.papierniak@gmail.com>
2024-04-22 10:03:00 -0500

    Play button

--- Mega Jump/StartScreen.swift
+++ Mega Jump/StartScreen.swift
@@ -11,6 +11,7 @@ import GameplayKit
 class StartScreen: SKScene, SKPhysicsContactDelegate {
     override func didMove(to view: SKView) {
         createBackground()
+        createButton()
     }
     func createBackground() {
         let imageName = "f0b8d6fc41d58e868b348cc33881093f.jpg"
@@ -24,8 +25,16 @@ class StartScreen: SKScene, SKPhysicsContactDelegate {
             starsBackground.zPosition = -1
             starsBackground.position = CGPoint(x: 0, y: starsBackground.size.height * CGFloat (i))
             addChild(starsBackground)
-            
         }
+    }
+    func createButton() {
+        let imageName = "F2C97135-A8F6-4995-9B1E-071E31B48D82"
+        let playbutt = SKTexture (imageNamed: imageName)
+        let play = SKSpriteNode (texture: playbutt, size: CGSize(width: 100, height: 60))
+        play.position = CGPoint (x: 0, y: -75)
+        play.physicsBody = SKPhysicsBody(rectangleOf: play.size)
+        play.physicsBody?.isDynamic = false
+        addChild(play)
     }
 }
 
